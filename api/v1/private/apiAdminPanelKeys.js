b57de174953704020afbf2ae26236815e3591f43
Alicia <gw17maleckimaksym@glow.sch.uk>
2018-09-09 23:50:22 +0100

    everything from old api

---
+++ api/v1/private/apiAdminPanelKeys.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const { query } = require("../../../objects/db");
+const config = require("../../../config.json");
+
+async function getAllFromDB() {
+    return await query("SELECT * FROM tokens WHERE allowed = 1")
+}
+
+app.get("/api/v1/getKeys", (req, res) => {
+    var key = req.query.k;
+    if (key != config.server.key) {
+        return res.end("Invalid key")
+    } else {
+        var arr = [];
+        getAllFromDB().then(q => {
+            q.forEach(key => {
+                const id = key.id;
+                const t = key.token
+                arr.push({id, t});
+            });
+            res.json(arr);
+        })
+    }
+})
+
+module.exports = app;
+
---
+++ api/v1/public/apiGetFiles.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const { query } = require("../../objects/db")
+
+async function getAllFromDB(u) {
+    const user = await query("SELECT * FROM users WHERE username = ?", u)
+    const token = user[0].token
+    return await query("SELECT * FROM files WHERE uploader_token = ?", token)
+}
+
+app.get("/api/v1/getFiles", (req, res) => {
+    var username = req.query.u;
+    var arr = [];
+    getAllFromDB(username).then(q => {
+        q.forEach(file => {
+            const id = file.id;
+            const fn = file.filename
+            arr.push({id, fn});
+        });
+        res.json(arr);
+    })
+})
+
+module.exports = app;
