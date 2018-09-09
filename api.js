769f503a40f24fa71ad68e65639f8d994257b94f
Alicia <gw17maleckimaksym@glow.sch.uk>
2018-09-10 00:01:14 +0100

    Forgot these, was rusing the release.

--- api.js
+++ api.js
@@ -1,3 +1,8 @@
 const express = require("express");
 const app = express();
+const config = require("./config.json")
 
+app.use(require("./api/v1/private/apiAdminPanelKeys"))
+app.use(require("./api/v1/public/apiGetFiles"))
+
+app.listen(config.server.port)
